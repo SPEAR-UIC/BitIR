42660a98e72392170ea7377b1b676859bda99004
Maisy Dunlavy <dunlavymm@gmail.com>
2026-07-22 00:34:49 +0000

    Add NVIDIA TRACE_DUE001 entropy full trace result

---
+++ divergence_analysis_outputs/nvidia_trace_TRACE_DUE001_20260722_003049/entropy/trace_site156_bit31/render_injected_ir.cmd
@@ -0,0 +1,1 @@
+/soft/compilers/llvm/release-17.0.6/bin/opt -S /var/tmp/pbs.7268154.polaris-pbs-01.hsn.cm.polaris.alcf.anl.gov/nvidia_divergence_entropy_156_31_7268154.polaris-pbs-01.hsn.cm.polaris.alcf.anl.gov/device.injected.bc -o /home/mdunlavy/GPU_FI/divergence_analysis_outputs/nvidia_trace_TRACE_DUE001_20260722_003049/entropy/trace_site156_bit31/device.injected.ll 
